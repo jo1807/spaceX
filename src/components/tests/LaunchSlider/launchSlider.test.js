622712452d428d00bf4368abc9c81b46ff9651ac
Joanne Notley <jkt70072@gmail.com>
2020-03-24 08:47:27 +0000

    initial test

---
+++ src/components/tests/LaunchSlider/launchSlider.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import RocketDetailsButton from "../../RocketSummary";
+import RocketSummary from "../../RocketSummary";
+import LaunchSlider from "../../LaunchSlider";
+import { launches } from "../mockData";
+
+afterEach(cleanup);
+
+it("renders correctly", () => {
+  const { asFragment } = render(<LaunchSlider launches={launches} />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("correctly shows next card in slider on click of next button", () => {
+  const { getByTestId, getByText, container } = render(
+    <LaunchSlider launches={launches} />
+  );
+  // console.log(
+  //   "console",
+  //   container.firstChild.childNodes[1]
+  //     .querySelector("div:nth-child(2)")
+  //     .firstChild.querySelector("div:nth-child(1)")
+  // );
+
+  // fireEvent.click(getByTestId("next-btn"));
+});
