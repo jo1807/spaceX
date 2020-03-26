2a17cf2a8fdd9c4ec25e23b66427f25a8c2d1712
Joanne Notley <jkt70072@gmail.com>
2020-03-26 20:39:29 +0000

    unit test next btn showing next card in slider

--- src/tests/LaunchSlider/launchSlider.test.js
+++ src/tests/LaunchSlider/launchSlider.test.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { render, cleanup, fireEvent } from "@testing-library/react";
-import RocketDetailsButton from "../../components/RocketSummary";
-import RocketSummary from "../../components/RocketSummary";
+import {
+  render,
+  cleanup,
+  fireEvent,
+  waitFor,
+  act
+} from "@testing-library/react";
 import LaunchSlider from "../../components/LaunchSlider";
 import { launches } from "../mockData";
 
@@ -12,16 +16,58 @@ it("renders correctly", () => {
   expect(asFragment()).toMatchSnapshot();
 });
 
-it("correctly shows next card in slider on click of next button", () => {
-  const { getByTestId, getByText, container } = render(
+it("correctly shows next card in slider on click of next button", async () => {
+  const { getByTestId, container } = render(
     <LaunchSlider launches={launches} />
   );
-  // console.log(
-  //   "console",
-  //   container.firstChild.childNodes[1]
-  //     .querySelector("div:nth-child(2)")
-  //     .firstChild.querySelector("div:nth-child(1)")
-  // );
+  const centerCardInLaunchSlider = container.firstChild.childNodes[1].querySelector(
+    "div:nth-child(2)"
+  );
+  const nextBtn = getByTestId("next-btn");
 
-  // fireEvent.click(getByTestId("next-btn"));
+  expect(centerCardInLaunchSlider.firstChild.textContent).toBe("TEST2");
+  expect(centerCardInLaunchSlider.childNodes[1].textContent).toBe(
+    "Flight Number: 21"
+  );
+  expect(centerCardInLaunchSlider.childNodes[2].textContent).toBe(
+    "Rocket Name: Falcon Q"
+  );
+
+  act(() => {
+    fireEvent.click(nextBtn);
+  });
+
+  fireEvent.click(nextBtn);
+
+  await waitFor(() => getByTestId("card-wrapper-3"));
+
+  const centerCardAfterNextBtnClick = getByTestId(
+    "card-wrapper-3"
+  ).querySelector("div:nth-child(2)");
+
+  expect(centerCardAfterNextBtnClick.firstChild.textContent).toBe("TEST3");
+  expect(centerCardAfterNextBtnClick.childNodes[1].textContent).toBe(
+    "Flight Number: 23"
+  );
+});
+
+it("disabled button to click next when it reached the last card", () => {
+  const { getByTestId } = render(<LaunchSlider launches={launches} />);
+
+  const nextBtn = getByTestId("next-btn");
+  expect(getByTestId("next-btn").disabled).toEqual(false);
+  act(() => {
+    fireEvent.click(nextBtn);
+  });
+
+  fireEvent.click(nextBtn);
+
+  expect(getByTestId("next-btn").disabled).toBe(true);
+});
+
+it("back button is disabled on initial render", () => {
+  const { getByTestId } = render(<LaunchSlider launches={launches} />);
+  const backBtn = getByTestId("back-btn");
+
+  expect(backBtn.disabled).toEqual(true);
 });
